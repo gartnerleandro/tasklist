ac772ca3efc6c83d05a01dcd9d2537a816014d71
gartnerleandro <gartnerleandro@gmail.com>
2020-02-22 19:36:45 +0100

    Fix: app test and add list to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import List from './components/list/List';
+
+const taks = [
+  {
+    id: 1,
+    title: 'fist',
+    body: 'fist body',
+  },
+  {
+    id: 2,
+    title: 'second',
+    body: 'second body',
+  },
+  {
+    id: 3,
+    title: 'third',
+    body: 'third body',
+  },
+];
 
 const App = () => (
-  <div>
+  <div className="app">
     <p>
       Hello World!
     </p>
+
+    <List elements={taks} />
   </div>
 );
 
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -7,10 +7,7 @@ describe('Tasklist app render', () => {
   it('render div that contains a p with the text Hello World on first load', () => {
     const wrapper = mount(<App />);
 
-    expect(wrapper.find('div').children()).toHaveLength(1);
-    expect(wrapper.find('div').childAt(0).type()).toBe('p');
-    expect(wrapper.find('p').childAt(0).html()).toBe('Hello World!');
-
-    wrapper.unmount();
+    expect(wrapper.find('.app').childAt(0).type()).toBe('p');
+    expect(wrapper.find('.app').childAt(0).html()).toBe('<p>Hello World!</p>');
   });
 });
